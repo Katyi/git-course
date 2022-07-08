688fc03c31c34fdf36a2cc02eeef2846d51351aa
Katyi <egorova.aleksandra@gmail.com>
2022-07-08 10:04:17 +0300

    added testing and building

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-document.body.innerHTML = '<h1>Hello from Javascript</h1>'
-console.log('Test !!!!!!!');
+// document.body.innerHTML = '<h1>Hello from Javascript</h1>'
+function sum(a, b) {
+  return a + b;
+}
+
+// console.log(sum);
+
+module.exports = sum;
